fae964a6727f5467561143e73eaeadf0d2ba2096
nimra <nimra.azmat@mlsense.ai>
2023-06-07 15:32:24 +0500

    Raw-Query

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -5,7 +5,6 @@ import pandas as pd
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 import psycopg2
-from sqlalchemy import create_engine
 from airflow.models import Variable
 from dotenv import load_dotenv
 
@@ -16,15 +15,6 @@ DB_USER = Variable.get("DATABASE_USER")
 DB_PASS = Variable.get("DATABASE_PASS")
 DB_PORT = Variable.get("DATABASE_PORT")
 DB_HOST = Variable.get("DATABASE_HOST")
-# DB_NAME = os.getenv("DATABASE_NAME")
-# DB_USER = os.getenv("DATABASE_USER")
-# DB_PASS = os.getenv("DATABASE_PASS")
-# DB_PORT = os.getenv("DATABASE_PORT")
-# DB_HOST = "cloudsql"
-
-# db_connection = (
-#     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@cloudsql:{DB_PORT}/{DB_NAME}"
-# )
 
 
 def song_table():
@@ -84,6 +74,82 @@ def read_csv_data():
     return df
 
 
+def insert_data_to_sql(**context):
+    """
+    Inserts the data from the DataFrame into the 'Song' table in the database.
+
+    This function takes a pandas DataFrame as input and inserts the data into
+    the 'Song' table in the specified database using raw SQL queries. The DataFrame
+    columns must match the table columns for a successful insertion.
+    """
+    df_read = context["ti"].xcom_pull(task_ids="read_csv_data")
+
+    # Create a SQLAlchemy engine with the connection string
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+    )
+    curs = conn.cursor()
+
+    # Generate the SQL query for bulk insertion
+    query = """
+            INSERT INTO Song_airflow (id_songs, name_song, album, album_id, artists, artist_ids, danceability, duration_ms) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
+            """
+
+    try:
+        # Convert DataFrame to a list of tuples
+        data = df_read.to_records(index=False).tolist()
+
+        # Execute the bulk insertion
+        curs.execute(query, data, multi=True)
+        print("Data inserted")
+    except Exception as e:
+        logging.error("Error during insertion of data: %s", str(e))
+
+    conn.commit()
+    curs.close()
+    conn.close()
+    logging.info("Data Populated")
+
+
+default_args = {
+    "owner": "airflow_dag",
+    "start_date": datetime(2023, 6, 5),
+    "schedule_interval": "0 0 * * *",
+    "catchup": False,
+}
+with DAG(
+    "airflow_task", default_args=default_args, schedule_interval="@daily", catchup=False
+) as dag:
+    """
+    This is the main dag which will start the airflow tasks
+    """
+    t_song_table = PythonOperator(task_id="create_table", python_callable=song_table)
+    t_read_csv = PythonOperator(task_id="read_csv_data", python_callable=read_csv_data)
+    # t_clean_data = PythonOperator(
+    #     task_id="clean_data", python_callable=data_cleaning, provide_context=True
+    # )
+
+    t_insert_data = PythonOperator(
+        task_id="data_into_db", python_callable=insert_data_to_sql, provide_context=True
+    )
+    # Set task dependencies e.g 1 -> 2 -> 3 task
+    t_song_table >> t_read_csv >> t_insert_data
+
+
+# DB_NAME = os.getenv("DATABASE_NAME")
+# DB_USER = os.getenv("DATABASE_USER")
+# DB_PASS = os.getenv("DATABASE_PASS")
+# DB_PORT = os.getenv("DATABASE_PORT")
+# DB_HOST = "cloudsql"
+
+# db_connection = (
+#     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@cloudsql:{DB_PORT}/{DB_NAME}"
+# )
 # def insert_data_to_sql(**context):
 #     """
 #     Inserts the data from the DataFrame into the 'Song' table in the database.
@@ -132,70 +198,44 @@ def read_csv_data():
 #     logging.info("Data Populated")
 
 
-def insert_data_to_sql(**context):
-    """
-    Inserts the data from the DataFrame into the 'Song' table in the database.
+# def insert_data_to_sql(**context):
+#     """
+#     Inserts the data from the DataFrame into the 'Song' table in the database.
 
-    This function takes a pandas DataFrame as input and inserts the data into
-    the 'Song' table in the specified database using raw SQL queries. The DataFrame
-    columns must match the table columns for a successful insertion.
-    """
-    df_read = context["ti"].xcom_pull(task_ids="read_csv_data")
+#     This function takes a pandas DataFrame as input and inserts the data into
+#     the 'Song' table in the specified database using raw SQL queries. The DataFrame
+#     columns must match the table columns for a successful insertion.
+#     """
+#     df_read = context["ti"].xcom_pull(task_ids="read_csv_data")
 
-    # Create a SQLAlchemy engine with the connection string
-    # conn = psycopg2.connect(
-    #     host=DB_HOST,
-    #     port=DB_PORT,
-    #     database=DB_NAME,
-    #     user=DB_USER,
-    #     password=DB_PASS,
-    # )
-    # curs = conn.cursor()
+#     # Create a SQLAlchemy engine with the connection string
+#     # conn = psycopg2.connect(
+#     #     host=DB_HOST,
+#     #     port=DB_PORT,
+#     #     database=DB_NAME,
+#     #     user=DB_USER,
+#     #     password=DB_PASS,
+#     # )
+#     # curs = conn.cursor()
 
-    # # Generate the SQL query for bulk insertion
-    # query = """
-    #         INSERT INTO Song_airflow (id_songs, name_song, album, album_id, artists, artist_ids, danceability, duration_ms)
-    #         VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
-    #         """
+#     # # Generate the SQL query for bulk insertion
+#     # query = """
+#     #         INSERT INTO Song_airflow (id_songs, name_song, album, album_id, artists, artist_ids, danceability, duration_ms)
+#     #         VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
+#     #         """
 
-    # Convert DataFrame to a list of tuples for bulk insertion
-    # records = df_read.to_records(index=False)
-    # data = list(records)
-    db_connection = (
-        f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    )
-    engine = create_engine(db_connection)
-    # Insert the data into the "Song" table
-    with engine.connect() as connection:
-        df_read.to_sql(
-            "Song_airflow", connection, if_exists="append", index=False, method="multi"
-        )
-    # Close the database connection
-    engine.dispose()
-    logging.info("Data Populated")
-
-
-# new DAG
-default_args = {
-    "owner": "airflow_dag",
-    "start_date": datetime(2023, 6, 5),
-    "schedule_interval": "0 0 * * *",
-    "catchup": False,
-}
-with DAG(
-    "airflow_task", default_args=default_args, schedule_interval="@daily", catchup=False
-) as dag:
-    """
-    This is the main dag which will start the airflow tasks
-    """
-    t_song_table = PythonOperator(task_id="create_table", python_callable=song_table)
-    t_read_csv = PythonOperator(task_id="read_csv_data", python_callable=read_csv_data)
-    # t_clean_data = PythonOperator(
-    #     task_id="clean_data", python_callable=data_cleaning, provide_context=True
-    # )
-
-    t_insert_data = PythonOperator(
-        task_id="data_into_db", python_callable=insert_data_to_sql, provide_context=True
-    )
-    # Set task dependencies e.g 1 -> 2 -> 3 task
-    t_song_table >> t_read_csv >> t_insert_data
+#     # Convert DataFrame to a list of tuples for bulk insertion
+#     # records = df_read.to_records(index=False)
+#     # data = list(records)
+#     db_connection = (
+#         f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+#     )
+#     engine = create_engine(db_connection)
+#     # Insert the data into the "Song" table
+#     with engine.connect() as connection:
+#         df_read.to_sql(
+#             "Song_airflow", connection, if_exists="append", index=False, method="multi"
+#         )
+#     # Close the database connection
+#     engine.dispose()
+#     logging.info("Data Populated")
